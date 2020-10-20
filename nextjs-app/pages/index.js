3a2842b5d268b1f36a1ea6b30cc00971bb5cd05d
erickmx <erickblanc96@gmail.com>
2020-10-20 10:15:29 -0500

    created SearchBar component created common directory inside components to create common components added SearchBar inside the index page

--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { Title } from '../components';
+import { Title, SearchBar } from '../components';
 
 export default function Home() {
   return (
     <div className="max-w-screen-xl mx-auto mt-8 outdoorsy__body">
       <Title title="Campervans" />
+      <SearchBar />
     </div>
   );
 }
